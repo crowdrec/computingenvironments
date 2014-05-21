c38dd6d48b0f739a5d98b7e63ca5b2318dd8795d
Davide <malagoli@gmail.com>
2014-05-21 17:20:10 +0200

    fixed execution order

--- 01.linux/01.centos/01.mahout/puppet/manifests/site.pp
+++ 01.linux/01.centos/01.mahout/puppet/manifests/site.pp
@@ -16,39 +16,38 @@
     # Install java
     class { 'jdk_oracle':
       version => '6',
+      platform => "x86",
       install_dir => '/opt/'
-    }
+    } ->
     file { '/opt/java':
       ensure => 'link',
       target => '/opt/java_home'
-    }
-
-
+    } ->
 
   # Install Maven
   class { "maven::maven":
     version => "3.2.1", # version to install
-    require => File['/opt/java'],
     # you can get Maven tarball from a Maven repository instead than from Apache servers, optionally with a user/password
     repo => {
       #url => "http://repo.maven.apache.org/maven2",
       #username => "",
       #password => "",
-    }
-  } 
+    },
+    require => Class["jdk_oracle"],
+    before => Exec["mvn clean compile assembly:single"]
+  } ->
 
   # Compile algorithm
   exec { "mvn clean compile assembly:single":
     cwd        => '/mnt/algo',
-    require => Package['maven::maven'],
     creates => "/mnt/algo/target/crowdrec-mahout-test-1.0-SNAPSHOT-jar-with-dependencies.jar",
-    path => ["/usr/bin", "/usr/sbin", "/bin", "/sbin"]
-  }
+    path => ["/usr/bin", "/usr/sbin", "/bin", "/sbin"],
+  } ->
 
   # Execute algorithm
   exec { "java -cp target/crowdrec-mahout-test-1.0-SNAPSHOT-jar-with-dependencies.jar dev.crowdrec.recs.mahout.ItembasedRec_batch /tmp /mnt/messaging/msg":
-    require => File['/mnt/algo/target/crowdrec-mahout-test-1.0-SNAPSHOT-jar-with-dependencies.jar'],
     cwd        => '/mnt/algo',
-    path => ["/usr/bin", "/usr/sbin", "/bin", "/sbin"]
+    path => ["/usr/bin", "/usr/sbin", "/bin", "/sbin"],
+    require => Exec["mvn clean compile assembly:single"]
   }
 
