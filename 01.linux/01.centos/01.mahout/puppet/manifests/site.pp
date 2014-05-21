d7ff8c89d12d087a22d49a0314b0430fa1376c28
Davide <malagoli@gmail.com>
2014-05-21 15:04:31 +0200

    first example vm

---
+++ 01.linux/01.centos/01.mahout/puppet/manifests/site.pp
@@ -0,0 +1,45 @@
+
+    # Install base packages
+    $enhancers = [ "wget", "unzip", "git" ]
+    package { $enhancers: }
+
+	 # Disable SELinux
+    class { 'selinux':
+        mode => 'disabled'
+    }
+    
+    # Disable iptables
+    class { 'firewall':
+        ensure => 'stopped'
+    }
+
+    # Install java
+    class { 'jdk_oracle':
+      version => '6',
+      install_dir => '/opt/'
+    }
+    file { '/opt/java':
+      ensure => 'link',
+      target => '/opt/java_home'
+    }
+
+
+
+  # Install Maven
+  class { "maven::maven":
+    version => "3.2.1", # version to install
+    # you can get Maven tarball from a Maven repository instead than from Apache servers, optionally with a user/password
+    repo => {
+      #url => "http://repo.maven.apache.org/maven2",
+      #username => "",
+      #password => "",
+    }
+  } 
+
+  # Compile algorithm
+  exec { "mvn clean compile assembly:single":
+    cwd        => '/mnt/algo',
+    creates => "/mnt/algo/target/crowdrec-mahout-test-1.0-SNAPSHOT-jar-with-dependencies.jar",
+    path => ["/usr/bin", "/usr/sbin", "/bin", "/sbin"]
+  }
+
