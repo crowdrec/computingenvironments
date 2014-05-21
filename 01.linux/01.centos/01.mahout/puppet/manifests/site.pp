97b43c4ff0a4ae8270ada27462426f30affd2ffc
Davide <davide.malagoli@moviri.com>
2014-05-21 16:18:24 +0200

    Update site.pp

--- 01.linux/01.centos/01.mahout/puppet/manifests/site.pp
+++ 01.linux/01.centos/01.mahout/puppet/manifests/site.pp
@@ -27,7 +27,9 @@
 
   # Install Maven
   class { "maven::maven":
+    require => "
     version => "3.2.1", # version to install
+    require => File['/opt/java']
     # you can get Maven tarball from a Maven repository instead than from Apache servers, optionally with a user/password
     repo => {
       #url => "http://repo.maven.apache.org/maven2",
@@ -39,12 +41,14 @@
   # Compile algorithm
   exec { "mvn clean compile assembly:single":
     cwd        => '/mnt/algo',
+    require => Package['maven::maven']
     creates => "/mnt/algo/target/crowdrec-mahout-test-1.0-SNAPSHOT-jar-with-dependencies.jar",
     path => ["/usr/bin", "/usr/sbin", "/bin", "/sbin"]
   }
 
   # Execute algorithm
   exec { "java -cp target/crowdrec-mahout-test-1.0-SNAPSHOT-jar-with-dependencies.jar dev.crowdrec.recs.mahout.ItembasedRec_batch /tmp /mnt/messaging/msg":
+    require => File['/mnt/algo/target/crowdrec-mahout-test-1.0-SNAPSHOT-jar-with-dependencies.jar'],
     cwd        => '/mnt/algo',
     path => ["/usr/bin", "/usr/sbin", "/bin", "/sbin"]
   }
