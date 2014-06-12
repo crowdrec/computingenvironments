88cc43defce4a2ef63297e4e82f532db358135ed
Davide <malagoli@gmail.com>
2014-06-12 13:55:25 +0200

    added mymedialite computing environment

---
+++ 01.linux/02.ubuntu/01.mymedialite/puppet/manifests/site.pp
@@ -0,0 +1,47 @@
+	include apt
+
+	#"mono-runtime",  "mono-xbuild", "mono-dmcs", "mono-devel", "mono-gmcs", 
+	#Apt::Ppa['ppa:directhex/monoxide'] 
+
+	
+
+    # Install base packages
+    $enhancers = [ "unzip", "git",  "libfile-slurp-perl", "mono-xbuild", "mono-gmcs" ]
+    package { $enhancers: }
+
+	apt::ppa { 'ppa:directhex/monoxide': 
+	} ->
+	package { ["mono-devel"]:
+    	ensure => "installed"
+	}
+    
+    #checkout mymedialite from repo
+   	git::repo{'mymedialite':
+ 		path   => '/opt/mymedialite',
+ 		source => 'https://github.com/zenogantner/mymedialite'
+	} ->
+
+	 # Compile mymedialite
+  	exec { "make all":
+    	cwd        => '/opt/mymedialite',
+    	creates => "/opt/mymedialite/lib/mymedialite/MyMediaLite.dll",
+    	path => ["/usr/bin", "/usr/sbin", "/bin", "/sbin"],
+    	timeout => 600
+  	} ->
+
+  	#checkout wraprec from repo
+	git::repo{'wraprec':
+ 		path   => '/opt/wraprec',
+ 		source => 'https://github.com/babakx/WrapRec'
+	} ->
+
+	# build wraprec
+	exec { "xbuild":
+    	cwd        => '/opt/wraprec',
+    	creates => "/opt/mymedialite/lib/mymedialite/MyMediaLite.dll",
+    	path => ["/usr/bin", "/usr/sbin", "/bin", "/sbin"],
+    	timeout => 600
+  	} 
+
+
+  	
